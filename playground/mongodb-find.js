391534ba0ec6206879e8a02aaa6a8a75a48e4ba6
Adam Love <adam.love127@gmail.com>
2018-09-04 16:24:44 +1000

    Add delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -27,16 +27,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
       //console.log('Unable to fetch todos', err)
     //})
 
-  db.collection('Users')
-    .find({
-      name: 'Adam'
-    })
-    .toArray()
-    .then((docs) => {
-      console.log(JSON.stringify(docs, undefined, 2))
-    }, err => {
-      console.log('Unable to fetch users', err)
-    })
+  //db.collection('Users')
+    //.find({
+      //name: 'Adam'
+    //})
+    //.toArray()
+    //.then((docs) => {
+      //console.log(JSON.stringify(docs, undefined, 2))
+    //}, err => {
+      //console.log('Unable to fetch users', err)
+    //})
 
 
   db.close()
