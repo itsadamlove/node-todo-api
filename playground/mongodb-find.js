d6e9949ded3b5e1abb75d36d98be904bdc5515ea
Adam Love <adam.love127@gmail.com>
2018-09-04 16:15:58 +1000

    Initial Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,43 @@
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server', err)
+  }
+  console.log('Connected to MongoDB server')
+
+  //db.collection('Todos')
+    //.find({
+      //_id: new ObjectID('5b8e1de673bc076f5b71e19b')
+    //})
+    //.toArray()
+    //.then((docs) => {
+      //console.log('Todos')
+      //console.log(JSON.stringify(docs, undefined, 2))
+    //}, err => {
+      //console.log('Unable to fetch todos', err)
+    //})
+
+  //db.collection('Todos')
+    //.find()
+    //.count()
+    //.then((count) => {
+      //console.log('Todos count:', count)
+    //}, err => {
+      //console.log('Unable to fetch todos', err)
+    //})
+
+  db.collection('Users')
+    .find({
+      name: 'Adam'
+    })
+    .toArray()
+    .then((docs) => {
+      console.log(JSON.stringify(docs, undefined, 2))
+    }, err => {
+      console.log('Unable to fetch users', err)
+    })
+
+
+  db.close()
+})
